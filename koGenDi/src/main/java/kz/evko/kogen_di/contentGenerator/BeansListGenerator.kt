254605511fd686b8d75c95dd758f8d20101a8a7c
genius <Evko90@Yandex.ru>
2024-12-16 21:42:39 +0500

    refactor

--- koGenDi/src/main/java/kz/evko/kogen_di/contentGenerator/BeansListGenerator.kt
+++ koGenDi/src/main/java/kz/evko/kogen_di/contentGenerator/BeansListGenerator.kt
@@ -13,10 +13,12 @@ class BeansListGenerator(
 
             appendLine("enum class KoGenBeans(val type: Class<*>, val bean: Any) {")
             beans.forEach {
-                val returnType = it.returnType!!.resolve().declaration
-                append("\t_${returnType.simpleName.asString()}(")
-                append("${returnType.packageName.asString()}.${returnType.simpleName.asString()}::class.java, ")
-                append("${it.packageName.asString()}.${it.simpleName.asString()}()),\n")
+                it.returnType?.let { type ->
+                    val returnType = type.resolve().declaration
+                    append("\t_${returnType.simpleName.asString()}(")
+                    append("${returnType.packageName.asString()}.${returnType.simpleName.asString()}::class.java, ")
+                    append("${it.packageName.asString()}.${it.simpleName.asString()}()),\n")
+                }
             }
 
             appendLine("}")
